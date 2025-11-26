47791b894de9c112b00ae4b58942ab9b83efb7fd
unknown <xazadox@gmail.com>
2025-11-25 22:01:01 -0500

    Actualizacion Super Usuario

--- build.sh
+++ build.sh
@@ -6,3 +6,15 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate
+
+if [ -z "$SUPERUSER_PASSWORD" ]; then
+    echo "ATENCIÓN: La variable SUPERUSER_PASSWORD no está configurada. Omite la creación del superusuario."
+else
+
+    python manage.py createsuperuser --noinput \
+        --username OscarAdmin \
+        --email oscarquiroga@gmail.com
+    
+
+    echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.get(username='OscarAdmin').set_password('$SUPERUSER_PASSWORD'); User.objects.get(username='OscarAdmin').save()" | python manage.py shell
+fi
